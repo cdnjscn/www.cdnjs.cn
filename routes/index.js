f27588077c60c0e13b9ce579069e05c0c6cb95be
翔歌 <kimwang.wx@alibaba-inc.com>
2014-03-18 22:03:44 +0800

    index

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,36 @@ _ = require('underscore'),
 mongoConf = require('../mongo.json');
 
 exports.index = function(req, res){
-	var tags = {
-		'pop': ['jquery','twitter-bootstrap','angular.js','underscore.js','backbone.js','zepto','seajs'],
-		'template': ['mustache.js','hogan.js','handlebars.js'],
-		'css': ['twitter-bootstrap','animate.css','1140','authy-forms.css'],
-		'lib': ['jquery','underscore.js','lodash.js','zepto','yui']
-	},
-	tag = req.params.tag || 'pop';
-	
+	var tags = [
+		{
+			tag:'pop',
+			text: '流行',
+			list: ['jquery','twitter-bootstrap','angular.js','underscore.js','backbone.js','zepto','seajs']
+		},
+		{
+			tag:'template',
+			text: '模版引擎',
+			list: ['mustache.js','hogan.js','handlebars.js']
+		},
+		{
+			tag:'css',
+			text:'样式',
+			list:['twitter-bootstrap','animate.css','1140','authy-forms.css']
+		},
+		{
+			tag:'lib',
+			text: '框架',
+			list: ['jquery','underscore.js','lodash.js','zepto','yui']
+		}
+	],
+	tag = req.params.tag || 'pop',
+	data = _.find(tags,function(v){
+  		return v.tag == tag;
+    }),
+	list = data.list;
 	mongoose.connect('mongodb://' + mongoConf.ip + ':' + mongoConf.port + '/cdnjs',function(err){
 	  if (err) return;
-	  Project.find({name:{'$in': tags[tag]  }},function(err,data){
+	  Project.find({name:{'$in': list  }},function(err,data){
 		  mongoose.disconnect();
 		  if (!data) {
 			  console.log(err);
@@ -24,8 +43,7 @@ exports.index = function(req, res){
 		  _.map(data,function(v){
 			  v.assets = _.first(v.assets);
 		  });
-		  
-		  res.render('index', { title: 'cdnjs.cn',list: data,tags: _.keys(tags) });
+		  res.render('index', { title: 'cdnjs.cn',list: data, tags:tags});
 	  });
 	});
 	
