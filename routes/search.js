088a0c9133712526e23dd1842efa45a90313601b
翔歌 <kimwang.wx@alibaba-inc.com>
2014-03-21 21:00:00 +0800

    search

--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,16 @@
-var Project = require('../models/project');
+var Project = require('../models/project'),
+	_ = require('underscore');
 
 exports.index = function(req, res) {
 	  Project.find({name: new RegExp(req.query.q,'i')},function(err,data){
+		  
+		  _.map(data,function(v){
+			  var version = v.version;
+			  v.assets = _.find(v.assets,function(item){
+				  return item.version == version;
+			  });
+		  });
+		  
 		  res.render('search',{title:'搜索',list:data});
 	  });
     
