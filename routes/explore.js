cfda1d436fb886c95cfb6e5847f693b385f2c7c2
翔歌 <kimwang.wx@alibaba-inc.com>
2014-04-01 23:23:29 +0800

    重构发现代码

--- routes/explore.js
+++ routes/explore.js
@@ -5,34 +5,49 @@ var cdnjscn = require('../models/cdnjscn'),
 	async = require('async');
 
 exports.index = function(req, res){
-	
-	cdnjscn.find({},{'_id':0}).sort({'g_fork':-1}).limit(100).exec(function(err,sortList){
-		var names = [],len = sortList.length,list = [];
-		for (var i = 0; i < len; i++) {
-			names.push(sortList[i].name);
+	async.parallel({
+		pager:function(callback){
+			callback(null,{});
+		},
+		list: function(callback){
+			async.waterfall([
+			    function(next){
+					cdnjscn.find({},{'_id':0}).sort({'g_fork':-1}).limit(20).exec(function(err,sortList){
+						var names = [],len = sortList.length;
+						for (var i = 0; i < len; i++) {
+							names.push(sortList[i].name);
+						}
+						next(null, names,sortList);
+					});
+			    },
+			    function(names, sortList){
+					var len = sortList.length,
+						list = [];
+					Project.find({ name: {'$in' : names } }).exec(function(err,data){
+						var item = null;
+						for (var i = 0; i < len; i++) {
+							item = _.find(data,function(v) {
+								return v.name == sortList[i].name;
+							});
+							item['g_star'] = sortList[i].g_star;
+							item['g_fork'] = sortList[i].g_fork;
+							list.push(item);
+						}
+			  		  	_.map(list,function(v){
+			  			  var version = v.version;
+			  			  v.assets = _.find(v.assets,function(item){
+			  				  return item.version == version;
+			  			  });
+			  			  reSort(v.assets[0].files,v);
+			  		  	});
+						// parallel
+						callback(null,list);
+						list = null;
+					});
+			    }
+			]);	
 		}
-		Project.find({ name: {'$in' : names } }).exec(function(err,data){
-			var item = null;
-			for (var i = 0; i < len; i++) {
-				item = _.find(data,function(v) {
-					return v.name == sortList[i].name;
-				});
-				item['g_star'] = sortList[i].g_star;
-				item['g_fork'] = sortList[i].g_fork;
-				list.push(item);
-			}
-			
-  		  	_.map(list,function(v){
-  			  var version = v.version;
-  			  v.assets = _.find(v.assets,function(item){
-  				  return item.version == version;
-  			  });
-  			  reSort(v.assets[0].files,v);
-  		  	});
-			res.render('explore',{title:'发现-cdnjs.cn',list:list});
-			list = null;
-			
-		});
+	},function(err,results){
+		res.render('explore',{title:'发现-cdnjs.cn',list:results.list});
 	});
-	
 };
