9363c0498c41a7902e23cc2029e91048a37451d1
翔歌 <kimwang.wx@alibaba-inc.com>
2014-04-08 22:06:29 +0800

    重构首页router代码

--- routes/index.js
+++ routes/index.js
@@ -1,62 +1,75 @@
 var Project = require('../models/project'),
 	reSort = require('../util').reSort,
-	_ = require('underscore');
+	_ = require('underscore'),
+	async = require('async');
 
-exports.index = function(req, res){
-	var tags = [
-		{
-			tag:'pop',
-			text: '流行',
-			list: ['jquery','twitter-bootstrap','angular.js','ember.js','underscore.js','backbone.js','zepto','seajs']
+exports.index = function(req, res) {
+	var tags = [{
+		tag: 'pop',
+		text: '流行',
+		list: ['jquery', 'twitter-bootstrap', 'angular.js', 'ember.js', 'underscore.js', 'backbone.js', 'zepto', 'seajs']
+	}, {
+		tag: 'mobile',
+		text: '移动端',
+		list: ['zepto', 'jquery-mobile', 'jo', 'swipe', 'iScroll', 'topcoat']
+	}, {
+		tag: 'responsive',
+		text: '响应式',
+		list: ['twitter-bootstrap', 'foundation', 'responsive-nav.js', 'skeleton', 'topcoat', 'respond.js']
+	}, {
+		tag: 'template',
+		text: '模版引擎',
+		list: ['mustache.js', 'hogan.js', 'handlebars.js']
+	}, {
+		tag: 'css',
+		text: '样式',
+		list: ['twitter-bootstrap', 'animate.css', 'less.js', 'authy-forms.css', 'stylus']
+	}, {
+		tag: 'lib',
+		text: '框架',
+		list: ['jquery', 'underscore.js', 'lodash.js', 'zepto']
+	}];
+
+	async.parallel({
+		total: function(callback) {
+			Project.count(function(err, num) {
+				callback(err, num);
+			});
 		},
-		{
-			tag:'mobile',
-			text: '移动端',
-			list: ['zepto','jquery-mobile','jo','swipe','iScroll','topcoat']
+		tags: function(callback) {
+			callback(null, tags);
 		},
-		{
-			tag:'responsive',
-			text: '响应式',
-			list: ['twitter-bootstrap','foundation','responsive-nav.js','skeleton','topcoat','respond.js']
+		list: function(callback) {
+			var tag = req.params.tag || 'pop',
+				data = _.find(tags, function(v) {
+					return v.tag == tag;
+				}),
+				list = data.list;
+			Project.find({
+				name: {
+					'$in': list
+				}
+			}).exec(function(err, data) {
+				if (!data) {
+					console.log(err);
+					res.send(500);
+					return;
+				}
+				_.map(data, function(v) {
+					var version = v.version,
+						files = null;
+					v.assets = _.find(v.assets, function(item) {
+						return item.version == version;
+					});
+					files = v.assets[0].files;
+					reSort(files, v);
+					v.hasExt = files.length > 2;
+				});
+				callback(err, data);
+			});
 		}
-		,
-		{
-			tag:'template',
-			text: '模版引擎',
-			list: ['mustache.js','hogan.js','handlebars.js']
-		},
-		{
-			tag:'css',
-			text:'样式',
-			list:['twitter-bootstrap','animate.css','less.js','authy-forms.css','stylus']
-		},
-		{
-			tag:'lib',
-			text: '框架',
-			list: ['jquery','underscore.js','lodash.js','zepto']
-		}
-	],
-	tag = req.params.tag || 'pop',
-	data = _.find(tags,function(v){
-  		return v.tag == tag;
-    }),
-	list = data.list;
-	Project.find({name:{'$in': list  }},function(err,data){
-		  if (!data) {
-			  console.log(err);
-			  res.send(500);
-			  return;
-		  }
-		  _.map(data,function(v){
-			  var version = v.version,files = null;
-			  v.assets = _.find(v.assets,function(item){
-				  return item.version == version;
-			  });
-			  files = v.assets[0].files;
-			  reSort(files,v);
-			  v.hasExt = files.length > 2;
-		  });
-		  
-		  res.render('index', { title: 'cdnjs.cn',list: data, tags:tags});
+	}, function(err, data) {
+		data.title = 'cdnjs.cn';
+		res.render('index', data);
 	});
 };
