168ca3c59891fb7a8bf9ffd70a922201dd220037
翔歌 <kimwang.wx@alibaba-inc.com>
2014-04-11 13:02:46 +0800

    gh-pages

--- models/cdnjscn.js
+++
@@ -1,8 +0,0 @@
-// 项目数据库
-var Schema = require('mongoose').Schema;
-var cdnjscn = new Schema({ 
-	  "name": String,
-	  'g_star': Number,
-	  'g_fork': Number
-},{collection: 'cdnjscn'});
-module.exports = db.model('cdnjscn', cdnjscn);
