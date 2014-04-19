c9719d7f90b27be781c92bae2c5124a1a47f90a5
翔歌 <kimwang.wx@alibaba-inc.com>
2014-04-19 22:41:08 +0800

    优化视觉效果

--- models/project.js
+++ models/project.js
@@ -10,7 +10,7 @@ var project = new Schema({
     "maintainers":[{"name": String,"email":String,"web": String}],
     "bugs": String,
     "licenses": [{"type":String,"url":String}],
-    "repositories": {"type": String,"url": String},
+    "repositories": [],
     "assets": [{"version": String,"files": [String]}]
 },{ collection: 'cdnjs' });
 module.exports = db.model('cdnjs', project);
