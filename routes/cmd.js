7c33cb2ad2eeba4d0e3ccc46e369153d785b5595
翔歌 <wxnet2013@gmail.com>
2014-07-06 23:36:56 +0800

    前端生态圈

--- routes/cmd.js
+++ routes/cmd.js
@@ -1,7 +1,7 @@
 exports.index = function(req, res, next) {
 	res.render('cmd', {
 		page: {
-			title: '生态圈 - cdnjs.cn',
+			title: '前端生态圈 - cdnjs.cn',
 			description: '浏览器端共享node模块，模块化才是前端的未来！',
 			keywords: 'CommonJS,CMD,模块化'
 		}
