991f2bdfe29cc4af822ecc48652f82a104f7fef4
翔歌 <wxnet2013@gmail.com>
2014-07-06 14:03:17 +0800

    format

--- routes/cmd.js
+++ routes/cmd.js
@@ -2,7 +2,8 @@ exports.index = function(req, res, next) {
 	res.render('cmd', {
 		page: {
 			title: '生态圈 - cdnjs.cn',
-			desc: '浏览器端共享node模块，模块化才是前端的未来！'
+			description: '浏览器端共享node模块，模块化才是前端的未来！',
+			keywords: 'CommonJS,CMD,模块化'
 		}
 	});
 };
